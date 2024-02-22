fc80fa6606c50772b5505670b4253f1922ce5943
Bourama coulibaly <bouramadevweb@gmail.com>
2024-02-22 19:20:14 +0100

    Description de vos modifications

--- Capentreprise/app_vaccins/models.py
+++ Capentreprise/app_vaccins/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 class ODS_Flux_total_dep(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): ODS
+
+    Returns:
+        _type_: _description_
+    """
     code_region = models.CharField(max_length=250)
-    libelle_region = models.CharField(max_length=250)
     code_departement = models.CharField(max_length=250)
+    libelle_region = models.CharField(max_length=250)
     libelle_departement = models.CharField(max_length=250)
     date_fin_semaine = models.CharField(max_length=250)
     type_de_vaccin = models.CharField(max_length=250)
@@ -12,4 +20,56 @@ class ODS_Flux_total_dep(models.Model):
     nb_doses = models.CharField(max_length=250)
     def __str__(self) :
         return self.code_region + self.libelle_region + self.code_departement + self.libelle_departement 
+class Dapartement (models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): Dapartement
+
+    Returns:
+        _type_:code_departement , libelle_departement
+    """ 
+    pk_departement =models.CharField(max_length=250)
+    code_departement =models.IntegerField()
+    code_region = models.IntegerField()
+    libelle_region = models.CharField(max_length=250)
+    libelle_departement = models.CharField(max_length=250)
+    def __str__(self) :
+        return self.code_departement + self.libelle_departement
+class D_type_vaccin(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): Vaccins
+
+    Returns:
+        _type_: le type du vaccin
+    """
+    pk_type_vaccin = models.CharField(primary_key=True,max_length=250)
+    def __str__(self):
+        return f'{self.pk_type_vaccin}'
+class D_dates(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): dates
+
+    Returns:
+        _type_: dates
+    """
+    date_fin_semaine = models.DateField(primary_key=True,max_length=250)
+    def __str__(self) :
+        return f'{self.date_fin_semaine}'
+class F_doses(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): F_doses du vaccin
+    """
+    pk_F_doses = models.CharField(primary_key=True,max_length=250)
+    nb_ucd = models.IntegerField()
+    nb_doses = models.IntegerField()
+    pk_type_vaccin = models.ForeignKey(D_type_vaccin, on_delete=models.CASCADE)
+    pk_date = models.ForeignKey(D_dates, on_delete=models.CASCADE)
+    
     
